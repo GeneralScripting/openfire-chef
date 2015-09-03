c475a6f17053196b9489bafc1df86270d134b9cb
Jan Schwenzien <jan@general-scripting.com>
2015-09-03 21:52:09 +0200

    version bump

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email 'gavin@leftbrained.co.uk'
 license          'Apache 2.0'
 description      'Installs Openfire Jabber server'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          '0.2.1'
+version          '0.2.2'
 
 supports         'ubuntu'
 supports         'centos'
